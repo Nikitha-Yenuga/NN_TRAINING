4bf5cef69bde1c4692ece02aba0e82475cad9ab3
Nikitha-Yenuga <nikitha9048@gmail.com>
2026-03-12 15:24:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def predict():
     result = classes[np.argmax(prediction)]
 
     return render_template("index.html", prediction=result)
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
